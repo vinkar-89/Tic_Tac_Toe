44c6a69bfba361179ee85ecca05cf23ca13fd975
Nidhay Reddy <96610602+vinkar-89@users.noreply.github.com>
2023-06-22 18:46:40 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,9 +1,5 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 """
+Tic_Tac_toe Simulation in Python
 Author: Nidhay Reddy
 """
 
